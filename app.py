681cd54584437c903ace215b325bb56f152df194
Bratsathish <91409436+Bratsathish@users.noreply.github.com>
2022-01-18 12:19:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,url_for,request
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+
+
+@app.route("/markat")
+def markat():
+    return render_template("markat.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("register.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+@app.route("/Register" ,methods=["POST","GET"])
+def Register():
+    if request.method=="POST":
+          name=request.form.get('name')
+          age=request.form.get('age')
+          address=request.form.get('address')
+          contact=request.form.get('contact')
+          email=request.form.get('email')
+          return render_template("result.html",name=name,age=age,address=address,contact=contact,email=email)
+     
+if __name__=="__main__":
+    app.run(debug=True)
